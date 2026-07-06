ab672d61c632a0f7434969e257085edc58ac1700
lukeskywlker19 <thelukeskywlker19@gmail.com>
2026-07-06 15:13:54 -0400

    Bump paper api dependency to 26.2. Bump SkyLib dependency to 2.0.2.0. Add 26.2 support. Update gradle wrapper to 9.6.1.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.lukesky19"
-version = "0.5.1.0"
+version = "0.5.2.0"
 
 repositories {
     mavenLocal()
@@ -28,10 +28,10 @@ repositories {
 
 dependencies {
     // Paper
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
 
     // SkyLib
-    compileOnly("com.github.lukesky19:SkyLib:2.0.0.0")
+    compileOnly("com.github.lukesky19:SkyLib:2.0.2.0")
 
     // Integration
     compileOnly("world.bentobox:bentobox:2.7.0-SNAPSHOT")
